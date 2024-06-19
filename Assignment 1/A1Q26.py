b221f2a5eb1d50314334c517b9b22545fcdea074
Shanvi <144417466+Shanvi2005@users.noreply.github.com>
2024-06-19 13:29:13 +0530

    Add files via upload

---
+++ Assignment 1/A1Q26.py
@@ -0,0 +1,10 @@
+data=input("enter text")
+if data.startswith("pre"):
+    print("it starts with pre")
+else:
+    print("it does not start with pre")
+
+if data.endswith("ed"):
+    print("it ends with ed")
+else:
+    print("it does not end with ed")
---
+++ Assignment 1/A1Q27.py
@@ -0,0 +1,5 @@
+a=input("enter a string")
+list1 =[]
+for i in a:
+    list1.append(i)
+print(list1)
